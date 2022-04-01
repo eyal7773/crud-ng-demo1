48ce21db8cc19a21bea86f55364b25cb827d55ac
Eyal Bardugo <taliandeyal@gmail.com>
2022-04-01 10:26:10 +0300

    Create app components + Routes + Login functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { FormsModule } from '@angular/forms';
+
+
+const appRoutes:Routes = [
+  {path:'', component:LoginComponent},
+  {path:'login', component:LoginComponent},
+  {path:'dashboard',component: DashboardComponent},
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  private readonly EMAIL_KEY = 'email';
+
+  isAuthenticated:boolean = false;
+  
+  constructor(private _router: Router) {
+
+   }
+
+  ngOnInit(): void {
+  }
+
+  onLogin(value:any) :void{
+    console.log(value);
+    if (value.email === 'abc@gmail.com' && value.password === 'a12345') {
+      this.isAuthenticated = true;
+      localStorage.setItem(this.EMAIL_KEY,value.email)
+      this._router.navigate(['dashboard']);
+    } else {
+      this.isAuthenticated = false;
+    }
+  }
+
+
+}
